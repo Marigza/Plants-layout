893a08d52818bc6d097ac156b9175addf2254e65
Marina Gapeyeva <marigola@mail.ru>
2023-01-31 10:52:13 +0300

    feat: create select city function

--- plants/script.js
+++ plants/script.js
@@ -2,6 +2,8 @@ console.log("Plants1 \n1.Вёрстка валидная +10 \n2.Вёрстка 
 
 console.log("Plants2 \n1.Вёрстка соответствует макету ширина 768рх +24 \n2.Вёрстка соответствует макету ширина 380рх +24\n3.Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки + 15 \n4.На ширине экрана 380рх и меньше реализовано адаптивное меню +22");
 
+console.log("Plants3 \n1.При нажатии на кнопки:Gargens,Lawn,Planting происходит смена фокуса на услугах в разделе service +50\n2.Accordion в секции prices который включает в себя 3 выпадающих списка об услугах и ценах + 50\n3.В разделе contacts реализован select с выбором городов.В зависимости от выбора пользователя появляется блок с адресом и телефоном офиса в определенном городе +25.");
+
 //burger menu
 
 (function () {
@@ -129,5 +131,92 @@ console.log("Plants2 \n1.Вёрстка соответствует макету 
     }
 
   }))
+
+}());
+
+// select city
+
+(function () {
+  
+  const selector = document.querySelector('.location__selection');
+  const variant = document.querySelector('.location__variant'); 
+  const cityItems = Array.from(document.querySelectorAll('.location__item'));
+  let adress = document.querySelector('.location__adress');
+  const location = document.querySelector('.contacts__locations');
+  const imageWoman = document.querySelector('.image-woman');
+  let callUs = document.querySelector('.adress__call-us');
+
+  selector.addEventListener('click', () => {
+    if (selector.classList.contains('location__selection_active')) {
+      removeSelectblock();
+    } else {
+      addSelectBlock();
+    }
+  });
+
+  cityItems.forEach(item => item.addEventListener('click', () => {
+    showSelectCity(item);
+    changeCity(item);
+  }));
+
+  function addSelectBlock() {
+    adress.classList.remove('location__adress_active');
+    variant.classList.add('location__variant_active');
+    selector.classList.add('location__selection_active');
+    location.classList.add('contacts__locations_no-pudding');
+    imageWoman.classList.add('image-woman_no-visible');
+  };
+
+  function removeSelectblock() {
+    adress.classList.remove('location__adress_active');
+    variant.classList.remove('location__variant_active');
+    selector.classList.remove('location__selection_active');
+    location.classList.remove('contacts__locations_no-pudding');
+    imageWoman.classList.remove('image-woman_no-visible');
+  };
+
+  function showSelectCity(item) {
+    variant.classList.remove('location__variant_active');
+    adress.classList.add('location__adress_active');
+    selector.firstElementChild.innerText = item.innerText;
+  };
+
+  function changeCity(item) { 
+    let city = document.querySelector('.adress__city-name');
+    let phone = document.querySelector('.adress__phone-number');
+    let office = document.querySelector('.adress__office-number');
+    
+    if (item.innerText === 'Canandaigua, NY') { 
+      city.innerText = 'Canandaigua, NY';
+      phone.innerText = '+1	585	393 0001';
+      office.innerText = '151 Charlotte Street';
+    };
+    if (item.innerText === 'New York City') {
+      city.innerText = 'New York City';
+      phone.innerText = '+1	212	456 0002';
+      office.innerText = '9 East 91st Street';
+    };
+    if (item.innerText === 'Yonkers, NY') {
+      city.innerText = 'Yonkers, NY';
+      phone.innerText = '+1	914	678 0003';
+      office.innerText = '511 Warburton Ave';
+    };
+    if (item.innerText === 'Sherrill, NY') {
+      city.innerText = 'Sherrill, NY';
+      phone.innerText = '+1	315	908 0004';
+      office.innerText = '14 WEST Noyes BLVD';
+    };
+
+    callUs.href = 'tel:' + phone.innerText.replaceAll(' ', '');
+    callUs.title = phone.innerText.replaceAll(' ', '');
+    //console.log(callUs.href);
+  };
+
+  function calling() {
+    callUs.addEventListener('click', () => {
+      alert(`You are going to call the ${callUs.href}`)
+    })
+  };
+  calling();
 
 }());
